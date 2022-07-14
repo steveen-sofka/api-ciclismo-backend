b065e4608816e63ead781c576a96b0dd02b23ecb
steveen-sofka <steveen.rodriguez@sofka.com.co>
2022-07-14 08:45:21 -0500

    creación del proyecto

---
+++ src/main/java/com/sofka/apiciclismobackend/ApiCiclismoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.sofka.apiciclismobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCiclismoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiCiclismoBackendApplication.class, args);
+	}
+
+}
